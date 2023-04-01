67a7dc92dfaa09897ee41c77d55da0749a6b5af1
louiscat <io@iooo.io>
2023-04-01 20:10:32 +0800

    init

---
+++ cmake-build-debug/CMakeFiles/BasicExcel.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BasicExcel.dir/BasicExcel.cpp.obj"
+  "libBasicExcel.a"
+  "libBasicExcel.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BasicExcel.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/BasicExcel.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for BasicExcel.
+# This may be replaced when dependencies are built.
